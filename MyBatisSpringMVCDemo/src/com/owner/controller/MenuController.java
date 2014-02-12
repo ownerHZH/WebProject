4103f0535e7742e1b44456c446d98b5b94ad5035
ownerHZH <owners.hzh@gmail.com>
2014-02-12 22:48:54 +0800

    menu

--- MyBatisSpringMVCDemo/src/com/owner/controller/MenuController.java
+++ MyBatisSpringMVCDemo/src/com/owner/controller/MenuController.java
@@ -1,5 +1,6 @@
 package com.owner.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -36,36 +37,42 @@ public class MenuController {
     @RequestMapping("/menu")
     public String getMenu() 
     {
-    	List<MenuLevelOne> menuLevelOneList=null;
-    	List<MenuLevelTwo> menuLevelTwoList=null;
-    	List<MenuLevelThree> menuLevelThreeList=null;
-    	MenuLevelOne menuLevelOne=new MenuLevelOne();
+    	List<MenuLevelOne> menuLevelOneList=new ArrayList<MenuLevelOne>();
+    	List<MenuLevelTwo> menuLevelTwoList=new ArrayList<MenuLevelTwo>();
+    	List<MenuLevelThree> menuLevelThreeList=new ArrayList<MenuLevelThree>();
+    	
+		MenuLevelOne menuLevelOne=new MenuLevelOne();
     	menuLevelOne.setRole_code(6);
 		menuLevelOneList=menuLevelOneDAO.getMenuLevelOne(menuLevelOne);
 		if(menuLevelOneList!=null&&menuLevelOneList.size()>0)
 		{
+			menuLevelThreeList=menuLevelThreeDAO.getMenuLevelThree(null);
+			
 			for(int i=0;i<menuLevelOneList.size();i++)
 			{
 				MenuLevelTwo menuLevelTwo=new MenuLevelTwo();
 				menuLevelTwo.setMenu_level_one_id(menuLevelOneList.get(i).getMenu_level_one_id());
-				menuLevelTwoList=menuLevelTwoDAO.getMenuLevelTwo(menuLevelTwo);
-				menuLevelOneList.get(i).setMenuLevelTwoList(menuLevelTwoList);
-			}
-			if(menuLevelTwoList!=null&&menuLevelTwoList.size()>0)
-			{	
-				for(int i=0;i<menuLevelTwoList.size();i++)
-				{
-					MenuLevelThree menuLevelThree=new MenuLevelThree();
-					menuLevelThree.setMenu_level_two_id(menuLevelTwoList.get(i).getMenu_level_two_id());
-					menuLevelThreeList=menuLevelThreeDAO.getMenuLevelThree(menuLevelThree);
-					for(int j=0;j<menuLevelThreeList.size();j++)
-					{
-						System.out.println("---three---"+menuLevelThreeList.get(j).toString());
+				menuLevelTwoList=menuLevelTwoDAO.getMenuLevelTwo(menuLevelTwo);	
+				if (menuLevelTwoList != null && menuLevelTwoList.size() > 0) {
+					for (int x = 0; x < menuLevelTwoList.size(); x++) {
+						List<MenuLevelThree> threeList = new ArrayList<MenuLevelThree>();
+						if(menuLevelThreeList!=null&&menuLevelThreeList.size()>0)
+						{
+							for (int y = 0; y < menuLevelThreeList.size(); y++) {
+								if (menuLevelTwoList.get(x).getMenu_level_two_id() == menuLevelThreeList
+										.get(y).getMenu_level_two_id()) {
+									threeList.add(menuLevelThreeList.get(y));
+								}							
+							}
+							menuLevelTwoList.get(x).setMenuLevelThreeList(
+									threeList);
+						}	
 					}
-					menuLevelTwoList.get(i).setMenuLevelThreeList(menuLevelThreeList);
+					menuLevelOneList.get(i).setMenuLevelTwoList(
+							menuLevelTwoList);
 				}
-			}
-		}
+			}			
+		}		
 		for(int i=0;i<menuLevelOneList.size();i++)
 		{
 			System.out.println("==++++++==="+menuLevelOneList.get(i).toString());
